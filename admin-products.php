391b7f8c9a2a81a3f5a6a12e92056ea87c6045a6
gilbertobreyerjunior <gilbertobreyer.jr@gmail.com>
2020-05-10 15:58:48 -0300

    Edit and delete product

--- admin-products.php
+++ admin-products.php
@@ -58,8 +58,9 @@ header("Location: /admin/products");
 
 });
 
+
 //iremos editar o nosso produto
-$app->get("/admin/products/create", function(){
+$app->get("/admin/products/:idproduct", function($idproduct){
 
     User::verifyLogin();
     
@@ -67,21 +68,77 @@ $app->get("/admin/products/create", function(){
 //iremos criar um novo produto 
 $product = new Product();
 
-$product->setData($_POST);
+//iremos carregar  esse objeto para ter certeza que ele existe la no banco de dados  e estamos dizendo que ele tem que ter inteiro
+$product->get((int)$idproduct);
 
-$product->save();
 
-header("Location: /admin/products");
+$page = new PageAdmin();
 
-    exit;
+
+//iremos passar os dados do produto para o template
+$page->setTpl("products-update", [
+    'product'=>$product->getValues()
+
+
+]);
 
 
 
 });
 
+//iremos editar o nosso produto
+$app->post("/admin/products/:idproduct", function($idproduct){
+
+    User::verifyLogin();
+    
+   
+//iremos criar um novo produto 
+$product = new Product();
+
+//iremos carregar  esse objeto para ter certeza que ele existe la no banco de dados  e estamos dizendo que ele tem que ter inteiro
+$product->get((int)$idproduct);
+
+//temos as informações que vem por post  o que é texto recebos pelo $_POST
+$product->setData($_POST);
+//e fazemos o save
+$product->save();
+
+//e iremos fazer o upload do arquivo //passamos o nome do nosso campo lá no input e o que e arquivo recebemos peo $_FILES
+$product->setPhoto($_FILES["file"]);
+
+
+//fazemos o redirect para a lista de produtos
+
+header('Location: /admin/products');
+
+exit;
+
+});
 
 
 
+//iremos excluir o nosso produto
+$app->get("/admin/products/:idproduct/delete", function($idproduct){
+
+    User::verifyLogin();
+    
+   
+//iremos criar um novo produto 
+$product = new Product();
+
+//iremos carregar  esse objeto para ter certeza que ele existe la no banco de dados  e estamos dizendo que ele tem que ter inteiro
+$product->get((int)$idproduct);
+
+//chamo o metodo delete
+
+$product->delete();
+
+//apos um redirect
+
+header('Location: /admin/products');
+exit;
+
+});
 
 
 ?>
--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -94,9 +94,101 @@ public function delete()
 
 }
 
+public function checkPhoto(){
+
+//se conter essa foto
+
+if (file_exists($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR .
+ "resource" . DIRECTORY_SEPARATOR . 
+ "site" . DIRECTORY_SEPARATOR .
+ "img" . DIRECTORY_SEPARATOR . 
+ "products" . DIRECTORY_SEPARATOR .  // se encontrar um arquivo com o id do produto na pasta products ira retornar esse caminho para foto
+ $this->getidproduct() . ".jpg" //concatenamos com o id do produto como o nome da foto sera o id do nosso produto e dizemos que o padrão das nossas fotos sera .jpg
+)) {
+      //então
+    $url = "/resource/site/img/products/" . $this->getidproduct() . ".jpg";//eu faço um return //aqui é url
+} else{ //se essa foto nao possuir
+
+    $url = "/resource/site/img/product.jpg";
+
+}
+//a gente retorna e seta dentro do objeto
+return $this->setdesphoto($url);
 
 
 
+
+
+}
+
+
+
+
+//iremos reescrever o metodo getValues pegar os valores dessa classe
+public function getValues(){
+//a gente reescreveu o metodo getValues porque eu não quero chamar toda hora o metodo checkPhoto, entao dessa maneira aqui quando eu chamo o getValues da classe produto ele faz o checkPhoto e faz o getValues padrão da classe model 
+//iremos criar um metodo para verificar se tem uma foto nesse produto, assim nao vem sem uma foto, assim vindo uma foto padrao
+$this->checkPhoto();
+
+//ira fazer o que a classe principal faz e ira retornar o values
+$values = parent::getValues();
+
+return $values;
+
+
+}
+
+
+public function setPhoto($file)
+{
+
+
+    //iremos detectar qual é o tipo da extensão do  arquivo que está sendo mandado agora
+            // o $extension ira receber atrib  do arquivo e o nome que está dentro dele 
+            //ele pegou o nome do arquivo onde tem ponto e fez um array dele 
+ $extension =explode('.', $file['name']);
+//a extensao e a ultima posicao que ele achou desse array e então ele ira so pegar as 3 ou 4  letras que e a extensao do nome do meu arquivo
+$extension = end($extension);
+//agora fazemos um switch no atrib extension
+
+switch ($extension) {
+
+    case "jpg":
+    case "jpeg":            //utilizamos a funcao da biblioteca GD e como parametro na funcao passamos o nome temporario tmp_name que esta no servidor
+         $image = imagecreatefromjpeg($file["tmp_name"]);
+    break; // irei fazer uma coisa caso seja um gif, e caso seja um png
+
+    case "gif": 
+        $image = imagecreatefromgif($file["tmp_name"]);
+    break;
+
+    case "png": 
+        $image = imagecreatefrompng($file["tmp_name"]);
+    break;
+
+
+}
+
+//appos colocamos onde queremos salvar o arquivo
+
+$dist = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR .
+ "resource" . DIRECTORY_SEPARATOR . 
+ "site" . DIRECTORY_SEPARATOR .
+ "img" . DIRECTORY_SEPARATOR . 
+ "products" . DIRECTORY_SEPARATOR .  // se encontrar um arquivo com o id do produto na pasta products ira retornar esse caminho para foto
+ $this->getidproduct() . ".jpg";
+//passamos
+
+//o nome da imagem como primeiro parametro da funcao, e segundo o destino da imagem
+imagejpeg($image, $dist);
+
+imagedestroy($image);
+
+//por fim para o dado ficar carregado, assim ira para memoria para o desPhoto
+
+$this->checkPhoto();
+
+}
 
 }
 
