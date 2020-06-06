6b907e5408561323b5509fe334c7534b3b2e6b86
gilbertobreyerjunior <gilbertobreyer.jr@gmail.com>
2020-06-06 11:24:34 -0300

    change customer password

--- admin-users.php
+++ admin-users.php
@@ -4,6 +4,72 @@ use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 
 
+
+
+$app->get("/admin/users/:iduser/password", function($iduser){
+
+	User::verifyLogin();
+	$user = new User;
+
+	$user->get((int)$iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-password", [
+		"user"=>$user->getValues(), //iremos passar os valores para o template
+		"msgError"=>User::getError(),
+		"msgSuccess"=>User::getSuccess()
+
+	]);
+
+
+
+});
+
+
+
+$app->post("/admin/users/:iduser/password", function($iduser){
+
+	User::verifyLogin();
+
+	if (!isset($_POST['despassword']) || $_POST['despassword']==='') {
+
+		User::setError("Preencha a nova senha.");
+		header("Location: /admin/users/$iduser/password");
+		exit;
+
+	}
+
+	if (!isset($_POST['despassword-confirm']) || $_POST['despassword-confirm']==='') {
+
+		User::setError("Preencha a confirmação da nova senha.");
+		header("Location: /admin/users/$iduser/password");
+		exit;
+
+	}
+
+	if ($_POST['despassword'] !== $_POST['despassword-confirm']) {
+
+		User::setError("Confirme corretamente as senhas.");
+		header("Location: /admin/users/$iduser/password");
+		exit;
+
+	}
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+//trocamos a senha
+	$user->setPassword(User::getPasswordHash($_POST['despassword']));
+
+	User::setSuccess("Senha alterada com sucesso.");
+
+	header("Location: /admin/users/$iduser/password");
+	exit;
+
+});
+
+
 //Rota para listar os usuarios
 $app->get('/admin/users', function() {
 
